b8437c00f9122084af2391b7f381b4c0b451c727
Bachan-kubi <felixghagra@gmail.com>
2023-03-13 19:01:26 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function showContent(page) {
+    if (page == "about") {
+      document.getElementById("content").innerHTML = "<h2>About Us</h2><p>Hi! I am Bachan Kubi, a Frontend Developer</p>";
+    } else if (page == "contact") {
+      document.getElementById("content").innerHTML = "<h2>Contact Us</h2><p>You can reach us at Mobile: +8801919735497, Email: bachankubi@gmail.com.</p>";
+    }
+  }
+  
