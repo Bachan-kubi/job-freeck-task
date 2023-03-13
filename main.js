b5eea0ad11248c4d62b4c4655230ae2d96455790
Bachan-kubi <felixghagra@gmail.com>
2023-03-13 19:10:05 +0600

    code rearranged

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
+const content = document.getElementById("content");
+
+
 function showContent(page) {
     if (page == "about") {
-      document.getElementById("content").innerHTML = "<h2>About Us</h2><p>Hi! I am Bachan Kubi, a Frontend Developer</p>";
+      content.innerHTML = "<h2>About Us</h2><p>Hi! I am Bachan Kubi, a Frontend Developer</p>";
     } else if (page == "contact") {
-      document.getElementById("content").innerHTML = "<h2>Contact Us</h2><p>You can reach us at Mobile: +8801919735497, Email: bachankubi@gmail.com.</p>";
+      content.innerHTML = "<h2>Contact Us</h2><p>You can reach us at Mobile: +8801919735497, Email: bachankubi@gmail.com.</p>";
     }
   }
+
   
